5be9dca51d74dab892ec7b2ced2ee1fd37f5a581
morkovka2019 <49154547+morkovka2019@users.noreply.github.com>
2021-01-21 12:47:08 +0200

    Add files via upload

---
+++ src/Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+    public static void main(String [] args){
+        //        int myInt; // declare the variable
+        //        myInt = 31; initialize the variable
+        int myInt = 31; // also the variable can be declared and initialized by one string
+        short myShort = 3200; // from -32000 to 32000
+        long myLong = 7654321;
+        double myDouble = 2345.756;
+        float myFloat = 2.3f;
+
+        char c = 'a';
+        boolean b = true; // or false
+        byte myByte = -128; //from -128 to 128
+
+        System.out.println(myInt);
+        System.out.println(myShort);
+    }
+
+
+}
